ab0c389aa72c9fcb079587e7673d828c30d1eb63
lc9900 <whynot_hm@hotmail.com>
2016-07-06 14:06:11 -0400

    added gts custom filter

---
+++ ansible_app/plugins/filter/gts.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# Custom ansible filters for gts core
+
+
+def gtsOpconCutFluff(output_list):
+    """ Cut out everything above ======= including ======== from opcon output """
+    result_list = []
+    separator = 0
+
+    for line in output_list:
+        if '========' in line:
+            separator = 1
+            continue
+
+        if separator:
+            result_list.append(line)
+
+    return result_list
+
+
+def gtsOpconResultCount(output_list):
+    """ Count the number of lines after ======= from opcon output """
+    result_count = 0
+    separator = 0
+    for line in output_list:
+        if '========' in line:
+            separator = 1
+            continue
+
+        if separator:
+            result_count += 1
+
+    return result_count
+
+class FilterModule(object):
+    ''' The FilterModule class defines a filters method 
+    that returns a dictionary with the name of the 
+    filter function and the function itself.
+    The FilterModule class is Ansible-specific code that
+    makes the Jinja2 filter available to Ansible.  '''
+
+    def filters(self):
+        return {
+            'gtsOpconCutFluff': gtsOpconCutFluff,
+            'gtsOpconResultCount': gtsOpconResultCount
+        }
