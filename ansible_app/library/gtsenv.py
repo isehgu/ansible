11138f4d73460e8ea7f853508c445a44f6725f41
lc9900 <whynot_hm@hotmail.com>
2016-07-13 12:40:21 -0400

    1st draft of gtsenv module

--- ansible_app/library/gtsenv.py
+++ ansible_app/library/gtsenv.py
@@ -1,4 +1,8 @@
 #! /usr/bin/env python
+from ansible.module_utils.basic import AnsibleModule
+import subprocess
+import shlex
+
 
 DOCUMENTATION = '''
     Run commands within a gts env.
@@ -6,33 +10,63 @@ DOCUMENTATION = '''
 '''
 
 EXAMPLE = '''
+    - name: Run command inside gtsenv
+      gtsenv: command=gtsCtrl status env_number=038
 
 '''
 
 
-from ansible.module_utils.basic import AnsibleModule
-import subprocess
-
 def main():
-    input_structure = {}
+    input_structure = {
+        'env_number': {'required': True, 'type': 'str'},
+        'command': {'required': True, 'type': 'str'}
+    }
 
     # Getting input from yml files
     module = AnsibleModule(
-        argument_spec = input_structure,
-        supports_check_mode = True
+        argument_spec=input_structure,
+        supports_check_mode=True
         )
 
     env_number = module.params['env_number']
     command = module.params['command']
+    real_command = command + "| gtsenv " + env_number
+    shlex_command = shlex.split(real_command)
+    # Now use subprocess, run the command, capture output, parse it,
+    # and interpret if it's successful then constructure resulting json
+    command_result = subprocess.Popen(
+        shlex_command, shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
 
-    # Actual command -- echo "command" | gtsenv env_number
-    # Need to make sure special characters are escaped correctly
-    real_command = "echo \"%s\" | gtsenv %s" % (command, env_number)
+    #######################
+    # Reading command results
+    line = ''
+    rc = ''
+    stderr = ''
+    stdout = ''
+    stdoutlines = []
+    while True:
+        line = command_result.stdout.readline()
+        rc = command_result.poll()
 
-    # Now use subprocess, run the command, capture output, parse it, and interpret if it's successful
-    # then constructure resulting json
+        if not line:
+            if rc is not None:
+                break
+        else:
+            stdoutlines.append(line)
 
-    result = {}
+    stderr = command_result.stderr.read()
+    stdout = ' '.join(stdoutlines)
+
+    result = {
+        'stdout': stdout,
+        'stdoutlines': stdoutlines,
+        'rc': rc,
+        'stderr': stderr,
+        'shlex_list': shlex_command
+    }
+
     module.exit_json(**result)
 
 if __name__ == '__main__':
