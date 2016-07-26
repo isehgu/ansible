d6a40f97bec08d265810d328a2f8d9b9ab33958d
lc9900 <whynot_hm@hotmail.com>
2016-07-26 16:53:42 -0400

    more fix

--- ansible_app/filter_plugins/common.py
+++ ansible_app/filter_plugins/common.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Custom ansible filters for linux. 
+# Custom ansible filters for linux.
 # Common ones that are not found in ansible
 
 
@@ -15,6 +15,7 @@ def cutLastLoginLine(output_list):
 
     return result_list
 
+
 def hasUniqueListItem(list1, list2):
     """ Is there any item in list1 that doesn't
     contain any of list2 items?
@@ -22,12 +23,11 @@ def hasUniqueListItem(list1, list2):
     contain any one of the list2 items
     No/False -- All items of list1 contains at least one item from
     list2"""
-    
     for item1 in list1:
         # We assume item1 is unique unless discovered in the inner loop
         unique = True
         for item2 in list2:
-            if item2 in item1
+            if item2 in item1:
                 # Item1 has item2, it's no longer unique
                 unique = False
                 break
@@ -36,7 +36,7 @@ def hasUniqueListItem(list1, list2):
         # then we move onto the next item1
         # if unique = True, then this particular item1 doesn't contain any of list2
         # then we return True because an unqiue list item has been found
-        if unique == True:
+        if unique:
             return unique
 
     # Last item1 processed, and unique is not True for that last item1
@@ -44,12 +44,9 @@ def hasUniqueListItem(list1, list2):
     return unique
 
 
-
-
-
 class FilterModule(object):
-    ''' The FilterModule class defines a filters method 
-    that returns a dictionary with the name of the 
+    ''' The FilterModule class defines a filters method
+    that returns a dictionary with the name of the
     filter function and the function itself.
     The FilterModule class is Ansible-specific code that
     makes the Jinja2 filter available to Ansible.  '''
