b3aa29bd0440dbc5266be5fe60b7bd03c394b34e
lc9900 <whynot_hm@hotmail.com>
2016-07-26 16:49:03 -0400

    new filter

--- ansible_app/filter_plugins/common.py
+++ ansible_app/filter_plugins/common.py
@@ -15,6 +15,38 @@ def cutLastLoginLine(output_list):
 
     return result_list
 
+def HasUniqueListItem(list1, list2)
+    """ Is there any item in list1 that doesn't
+    contain any of list2 items?
+    Yes/True - There is at least one item in list1 that doesnt
+    contain any one of the list2 items
+    No/False -- All items of list1 contains at least one item from
+    list2"""
+    
+    for item1 in list1:
+        # We assume item1 is unique unless discovered in the inner loop
+        unique = True
+        for item2 in list2:
+            if item2 in item1
+                # Item1 has item2, it's no longer unique
+                unique = False
+                break
+
+        # if unique = False, then this specific item1 contains at least 1 of list2
+        # then we move onto the next item1
+        # if unique = True, then this particular item1 doesn't contain any of list2
+        # then we return True because an unqiue list item has been found
+        if unique == True:
+            return unique
+
+    # Last item1 processed, and unique is not True for that last item1
+    # So just return unique, which is false
+    return unique
+
+
+
+
+
 class FilterModule(object):
     ''' The FilterModule class defines a filters method 
     that returns a dictionary with the name of the 
@@ -25,4 +57,5 @@ class FilterModule(object):
     def filters(self):
         return {
             'cutLastLoginLine': cutLastLoginLine,
+            'HasUniqueListItem': HasUniqueListItem,
         }
